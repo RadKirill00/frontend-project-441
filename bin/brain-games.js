91e4f06229c268801b9a4b7eac957eeec351b272
Kirill <radkirill00@mail.ru>
2023-11-13 09:35:54 +0300

    доделал общую структуру

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { playBrainEven } from '../games/brain-even';
+import { playBrainCalc } from '../games/brain-calc';
+import { playBrainGCD } from '../games/brain-gcd';
+import { playBrainProgression } from '../games/brain-progression';
+import { playBrainPrime } from '../games/brain-prime';
 
-import { welcomeUser } from '../src/cli.js';
+console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!\n`);
 
+// Play each game
+playBrainEven();
+playBrainCalc();
+playBrainGCD();
+playBrainProgression();
+playBrainPrime();
 
-welcomeUser();
 
 
